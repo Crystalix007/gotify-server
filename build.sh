b8736295ba88b12adba2b2267e04c66b95bcb3a1
Michael Kuc <michaelkuc6@gmail.com>
2020-05-10 12:44:15 +0100

    Reduce amount packaged in Docker image.
    Do `npm install` before attempting to use the dependencies.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-(cd ui && yarn build)
+(cd ui && npm install && yarn build)
 packr
 export LD_FLAGS="-w -s -X main.Version=$(git describe --tags | cut -c 2-) -X main.BuildDate=$(date "+%F-%T") -X main.Commit=$(git rev-parse --verify HEAD) -X main.Mode=prod";
 go build -ldflags="$LD_FLAGS" -o gotify-app
