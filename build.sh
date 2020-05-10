6b35bcd1597e84d1637ef61024323242f4364097
Michael Kuc <michaelkuc6@gmail.com>
2020-05-10 10:43:55 +0100

    Add required UI html serving directory to Dockerfile.

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 (cd ui && yarn build)
 packr
 export LD_FLAGS="-w -s -X main.Version=$(git describe --tags | cut -c 2-) -X main.BuildDate=$(date "+%F-%T") -X main.Commit=$(git rev-parse --verify HEAD) -X main.Mode=prod";
-go build -ldflags="$LD_FLAGS" -o gotify-server
+go build -ldflags="$LD_FLAGS" -o gotify-app
